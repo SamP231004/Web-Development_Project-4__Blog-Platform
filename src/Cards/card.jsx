f12c01a31a67d2d5a4f752684e4d5da57457182a
Samarth Patel <samp231004@gmail.com>
2024-08-07 22:20:52 +0530

    third commit

--- src/Cards/card.jsx
+++ src/Cards/card.jsx
@@ -1,48 +1,59 @@
+import image7 from 'src/Images_Used/image_7.jpg';
+import image9 from 'src/Images_Used/image_9.jpg';
+import image4 from 'src/Images_Used/image_4.jpg';
+import image3 from 'src/Images_Used/image_3.png';
+import image2 from 'src/Images_Used/image_2.jpg';
+import image5 from 'src/Images_Used/image_5.jpg';
+import image10 from 'src/Images_Used/image_10.png';
+import image6 from 'src/Images_Used/image_6.jpg';
+import image8 from 'src/Images_Used/image_8.jpeg';
+
 const cards = [
     {
-        title : "The Super-Soldier Who Became a Symbol of Valor and Justice",
-        name : "Captain America",
-        image : "src/Images_Used/image_7.jpg"
-    }, 
-    {
-        title : "The Sorcerer Supreme Who Masters the Mystical Arts to Defend the Multiverse",
-        name : "Doctor Strange",
-        image : "src/Images_Used/image_9.jpg"
-    },   
-    {
-        title : "The Time-Travelling Tyrant with a Vision to Rewrite History",
-        name : "Kang",
-        image : "src/Images_Used/image_4.jpg"
+        title: "The Super-Soldier Who Became a Symbol of Valor and Justice",
+        name: "Captain America",
+        image: image7
     },
     {
-        title : "The Unlikely Hero Who Turned Chaos into Comedy and Courage",
-        name : "DeadPool",
-        image : "src/Images_Used/image_3.png"
+        title: "The Sorcerer Supreme Who Masters the Mystical Arts to Defend the Multiverse",
+        name: "Doctor Strange",
+        image: image9
     },
     {
-        title : "The Villain Who Nearly Ended Reality",
-        name : "Doctor Doom",
-        image : "src/Images_Used/image_2.jpg"
+        title: "The Time-Travelling Tyrant with a Vision to Rewrite History",
+        name: "Kang",
+        image: image4
     },
     {
-        title : "The Legendary God of Thunder Who Bridged Realms and Defended the Cosmos",
-        name : "Thor",
-        image : "src/Images_Used/image_5.jpg"
+        title: "The Unlikely Hero Who Turned Chaos into Comedy and Courage",
+        name: "DeadPool",
+        image: image3
     },
     {
-        title : "The Enigmatic Sorceress Who Wields Chaos Magic and Alters Reality",
-        name : "Loki",
-        image : "src/Images_Used/image_10.png"
+        title: "The Villain Who Nearly Ended Reality",
+        name: "Doctor Doom",
+        image: image2
     },
     {
-        title : "The Genius Inventor Who Transformed Into a Techno-Hero and Global Savior",
-        name : "Iron Man",
-        image : "src/Images_Used/image_6.jpg"
+        title: "The Legendary God of Thunder Who Bridged Realms and Defended the Cosmos",
+        name: "Thor",
+        image: image5
     },
     {
-        title : "The Enigmatic Sorceress Who Wields Chaos Magic and Alters Reality",
-        name : "Scarlet Witch",
-        image : "src/Images_Used/image_8.jpeg"
+        title: "The Enigmatic Sorceress Who Wields Chaos Magic and Alters Reality",
+        name: "Loki",
+        image: image10
+    },
+    {
+        title: "The Genius Inventor Who Transformed Into a Techno-Hero and Global Savior",
+        name: "Iron Man",
+        image: image6
+    },
+    {
+        title: "The Enigmatic Sorceress Who Wields Chaos Magic and Alters Reality",
+        name: "Scarlet Witch",
+        image: image8
     },
 ]
-export {cards}
+
+export { cards };
